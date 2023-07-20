6ef4b458e0b147c84b0e2f883ee691b1c3dd9360
Winston Durand <me@winstondurand.com>
2023-07-20 03:52:02 -0700

    save progress on converting to protos

--- client.go
+++ client.go
@@ -4,6 +4,9 @@ import (
 	"encoding/json"
 	"io"
 	"net/http"
+
+	"google.golang.org/protobuf/encoding/protojson"
+	"google.golang.org/protobuf/proto"
 )
 
 const (
@@ -46,6 +49,14 @@ func callApi[T any](c *Client, path string) (*T, error) {
 		return nil, err
 	}
 	v := new(T)
+
+	switch v.(type) {
+	case proto.Message:
+		err = protojson.Unmarshal(data, v)
+	default:
+		err = json.Unmarshal(data, v)
+	}
+
 	if err := json.Unmarshal(data, v); err != nil {
 		return nil, err
 	}
@@ -65,42 +76,6 @@ func (c *Client) Lifetime() (*Lifetime, error) {
 // Version returns the version info of the wallconnector.
 func (c *Client) Version() (*Version, error) {
 	return callApi[Version](c, versionPath)
-}
-
-// Vitals represents the current state of the wallconnector.
-//
-// Note: Several fields like SessionS, and UptimeS are probably ints, but
-// they're it's impossible to say parsing JSON from the API.
-//
-// See Wall Monitor FAQ for more details:
-// https://wallmonitor.app/faq/explain_technical
-type Vitals struct {
-	ContactorClosed   bool    `json:"contactor_closed" wc:"contactor_closed_state,gauge"`
-	VehicleConnected  bool    `json:"vehicle_connected" wc:"vehicle_connected_state,gauge"`
-	SessionS          float64 `json:"session_s" wc:"session_seconds,counter"`
-	GridV             float64 `json:"grid_v" wc:"grid_volts,gauge"`
-	GridHz            float64 `json:"grid_hz" wc:"grid_hertz,gauge"`
-	VehicleCurrentA   float64 `json:"vehicle_current_a" wc:"-"`
-	CurrentA_a        float64 `json:"currentA_a" wc:"-"`
-	CurrentB_a        float64 `json:"currentB_a" wc:"-"`
-	CurrentC_a        float64 `json:"currentC_a" wc:"-"`
-	CurrentN_a        float64 `json:"currentN_a" wc:"-"`
-	VoltageA_v        float64 `json:"voltageA_v" wc:"-"`
-	VoltageB_v        float64 `json:"voltageB_v" wc:"-"`
-	VoltageC_v        float64 `json:"voltageC_v" wc:"-"`
-	RelayCoilV        float64 `json:"relay_coil_v" wc:",gauge"`
-	PcbaTempC         float64 `json:"pcba_temp_c" wc:",gauge"`
-	HandleTempC       float64 `json:"handle_temp_c" wc:",gauge"`
-	McuTempC          float64 `json:"mcu_temp_c" wc:",gauge"`
-	UptimeS           float64 `json:"uptime_s" wc:",counter"`
-	InputThermopileUv float64 `json:"input_thermopile_uv" wc:",gauge"`
-	ProxV             float64 `json:"prox_v" wc:",gauge"`
-	PilotHighV        float64 `json:"pilot_high_v" wc:",gauge"`
-	PilotLowV         float64 `json:"pilot_low_v" wc:"pilot_low_volts,gauge"`
-	SessionEnergyWh   float64 `json:"session_energy_wh" wc:"session_energy_joules,counter"`
-	ConfigStatus      int     `json:"config_status"`
-	EvseState         int     `json:"evse_state"`
-	CurrentAlerts     []int   `json:"current_alerts"`
 }
 
 // Lifetime represents the lifetime stats of the wallconnector.
--- metrics.go
+++ metrics.go
@@ -1,80 +1,147 @@
 package wallconnector
 
 import (
+	"strings"
+
 	"github.com/prometheus/client_golang/prometheus"
+	"google.golang.org/protobuf/proto"
 )
 
 // A prometheus.Collector implementation for wallconnector stats.
-type vitalsCollector struct {
-	client *Client
+type collector struct {
+	client     *Client
+	metricSets map[string]metricSet
 }
 
-func (c *vitalsCollector) Describe(ch chan<- *prometheus.Desc) {
-	prometheus.DescribeByCollect(c, ch)
+func (c *collector) Describe(ch chan<- *prometheus.Desc) {
+	for _, set := range c.metricSets {
+		for _, metric := range set {
+			ch <- metric.desc
+		}
+	}
 }
 
-var (
-	// Vitals
-	vitalsCurrentAmps = prometheus.NewDesc(
-		"current_amps",
-		"Wall connector output in amps",
-		[]string{"phase"},
-		nil,
-	)
-	vitalsCurrentVolts = prometheus.NewDesc(
-		"current_volts",
-		"Wall connector output in volts",
-		[]string{"phase"},
-		nil,
-	)
-	vitalsGridFrequency = prometheus.NewDesc(
-		"grid_frequency",
-		"Grid frequency in hertz",
-		nil,
-		nil,
-	)
-	vitalsGridVoltage = prometheus.NewDesc(
-		"grid_voltage",
-		"Grid voltage in volts",
-		nil,
-		nil,
-	)
-	vitalsSessionSeconds = prometheus.NewDesc(
-		"session_seconds",
-		"Duration of the most recent charging session in seconds",
-		nil,
-		nil,
-	)
-	vitalsUptimeSeconds = prometheus.NewDesc(
-		"uptime_seconds",
-		"Duration of the wall connector uptime in seconds",
-		nil,
-		nil,
-	)
-)
+func (c *collector) Collect(ch chan<- prometheus.Metric) {
+	for _, set := range c.metricSets {
+		for name, metric := range set {
+			v, err := c.client.callApi(name)
+			if err != nil {
+				ch <- prometheus.NewInvalidMetric(metric.desc, err)
+				continue
+			}
 
-func (c *vitalsCollector) Collect(ch chan<- prometheus.Metric) {
-	if data, err := c.client.Vitals(); err != nil {
-		ch <- prometheus.MustNewConstMetric(vitalsCurrentAmps, prometheus.GaugeValue, data.CurrentA_a, "A")
-		ch <- prometheus.MustNewConstMetric(vitalsCurrentAmps, prometheus.GaugeValue, data.CurrentB_a, "B")
-		ch <- prometheus.MustNewConstMetric(vitalsCurrentAmps, prometheus.GaugeValue, data.CurrentC_a, "C")
-		ch <- prometheus.MustNewConstMetric(vitalsCurrentAmps, prometheus.GaugeValue, data.CurrentN_a, "N")
-		ch <- prometheus.MustNewConstMetric(vitalsCurrentVolts, prometheus.GaugeValue, data.VoltageA_v, "A")
-		ch <- prometheus.MustNewConstMetric(vitalsCurrentVolts, prometheus.GaugeValue, data.VoltageB_v, "B")
-		ch <- prometheus.MustNewConstMetric(vitalsCurrentVolts, prometheus.GaugeValue, data.VoltageC_v, "C")
-		ch <- prometheus.MustNewConstMetric(vitalsGridFrequency, prometheus.GaugeValue, data.GridHz)
-		ch <- prometheus.MustNewConstMetric(vitalsGridVoltage, prometheus.GaugeValue, data.GridV)
-		ch <- prometheus.MustNewConstMetric(vitalsSessionSeconds, prometheus.CounterValue, data.SessionS)
-		ch <- prometheus.MustNewConstMetric(vitalsUptimeSeconds, prometheus.CounterValue, data.UptimeS)
+			for _, value := range metric.metric.GetValue() {
+				ch <- prometheus.MustNewConstMetric(
+					metric.desc,
+					metric.typ,
+					value,
+					metric.labels...,
+				)
+			}
+		}
 	}
 }
 
 // NewCollector creates a new collector for wallconnector stats.
-func NewVitalsCollector(client *Client) prometheus.Collector {
-	return &vitalsCollector{
+func NewCollector(client *Client) prometheus.Collector {
+	return &collector{
 		client: client,
+		metricSets: map[string]metricSet{
+			vitalsPath: newMetricSet(new(Vitals), "vitals"),
+			// newMetricSet(new(Lifetime), "lifetime"),
+		},
 	}
 }
 
 // Ensure we implement the [prometheus.Collector] interface
-var _ prometheus.Collector = (*vitalsCollector)(nil)
+var _ prometheus.Collector = (*collector)(nil)
+
+type metricData struct {
+	typ    prometheus.ValueType
+	labels []string
+	desc   *prometheus.Desc
+	metric *Metric
+}
+
+type metricSet map[string]metricData
+
+func newMetricSet(v proto.Message, ns string) metricSet {
+	// Parse the Metric proto message off of each field on a Vitals message.
+	// It's fine to use reflection here since this is only called once at
+	// startup.
+	// Use proto reflect to parse the prometheus metric name and description
+	// from the proto message.
+	set := make(metricSet)
+	descs := make(descriptions)
+
+	desc := v.ProtoReflect().Descriptor()
+	for i := 0; i < desc.Fields().Len(); i++ {
+		field := desc.Fields().Get(i)
+		ext, ok := proto.GetExtension(field.Options(), E_Prometheus).(*Metric)
+		if !ok || ext.GetName() == "" {
+			continue
+		}
+
+		metric := metricData{
+			metric: ext,
+			desc:   descs.getDescription(ext, ns),
+			labels: ext.LabelValues(),
+		}
+
+		switch ext.GetType() {
+		case Metric_COUNTER:
+			metric.typ = prometheus.CounterValue
+		case Metric_GAUGE:
+			metric.typ = prometheus.GaugeValue
+		default:
+			panic("unknown metric type")
+		}
+
+		name := field.JSONName()
+		set[name] = metric
+	}
+
+	return set
+}
+
+type descriptions map[string]*prometheus.Desc
+
+func (m *Metric) LabelKeys() []string {
+	keys := make([]string, 0, len(m.GetLabels()))
+	for _, label := range m.GetLabels() {
+		key, _, ok := strings.Cut(label, ":")
+		if !ok {
+			panic("invalid label " + label)
+		}
+		keys = append(keys, key)
+	}
+	return keys
+}
+
+func (m *Metric) LabelValues() []string {
+	values := make([]string, 0, len(m.GetLabels()))
+	for _, label := range m.GetLabels() {
+		_, value, ok := strings.Cut(label, ":")
+		if !ok {
+			panic("invalid label " + label)
+		}
+		values = append(values, value)
+	}
+	return values
+}
+
+func (d descriptions) getDescription(v *Metric, ns string) *prometheus.Desc {
+	name := prometheus.BuildFQName("wallconnector", ns, v.GetName())
+	if desc, ok := d[name]; ok {
+		return desc
+	}
+
+	desc := prometheus.NewDesc(
+		name,
+		v.GetHelp(),
+		v.LabelKeys(),
+		nil,
+	)
+	d[name] = desc
+	return desc
+}
