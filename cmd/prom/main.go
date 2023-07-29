bccac610e388d88b44eda4f1b9fe94a49a3c9f34
Winston Durand <me@winstondurand.com>
2023-07-29 13:18:09 -0700

    Save work

--- cmd/prom/main.go
+++ cmd/prom/main.go
@@ -5,7 +5,10 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/R167/wallconnector"
+	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
@@ -16,20 +19,29 @@ var (
 )
 
 func main() {
+	start := time.Now()
 	// Listen on the specified address and serve prometheus metrics
 	// from the wall connector target.
 	flag.Parse()
 
 	// Create a new client for the wall connector.
-	client, err := NewClient(*target)
+	client, err := wallconnector.NewClient(*target)
 	if err != nil {
 		panic(err)
 	}
 
 	// Create a new collector for the wall connector.
-	collector := NewCollector(client)
+	collector := wallconnector.NewCollector(client)
 
-	// Register the collector with the prometheus default registry.
-	http.Handle(*path, promhttp.Handler())
-	log.Fatal(http.ListenAndServe(*addr, nil))
+	reg := prometheus.NewRegistry()
+	reg.MustRegister(collector)
+
+	// Serve the metrics on the specified path.
+	http.Handle(*path, promhttp.HandlerFor(reg, promhttp.HandlerOpts{
+		ProcessStartTime: start,
+	}))
+	log.Printf("listening on %s", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		panic(err)
+	}
 }
